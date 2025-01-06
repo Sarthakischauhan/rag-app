a44173f609e54d96b16b968fbfc5893506280820
Sarthakischauhan <sarthakchauhann@gmail.com>
2025-01-07 04:29:48 +0530

    retrieval and generation done!

--- main.py
+++ main.py
@@ -35,4 +35,48 @@ all_splits = splitter.split_documents(docs)
 print(f"splitted chunks are a total of {len(all_splits)}")
 
 # Storing these chunks in our vector database
+# Each chunk will have a seperate doc id in the vector database
 doc_ids = store.add_documents(documents=all_splits)
+
+# Retrieval and generation
+from langchain import hub
+
+prompt = hub.pull("rlm/rag-prompt")
+exm = prompt.invoke(
+    {"context": "(context goes here)", "question": "(question goes here)"}
+).to_messages()
+
+initial_prompt_message = exm[0].content
+
+# Defining the state of the application or the type of data our app plays with
+from langchain_core.documents import Document
+from typing_extensions import List, TypedDict
+class State(TypedDict):
+    question: str
+    context: List[Document]
+    answer: str
+
+# Our main two functions that get it done
+
+def generate(state):
+    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+    messages = prompt.invoke({"question":state["question"], "context":docs_content})
+    response = llm_model.invoke(messages)
+    return {"answer":response.content}
+
+def retrieve(state):
+    retrieved_docs = store.similarity_search(state["question"])
+    return {"context":retrieved_docs}
+
+# Compile our pipeline into a graph
+from langgraph.graph import START, StateGraph
+
+graph_builder = StateGraph(State).add_sequence([retrieve, generate])
+graph_builder.add_edge(START, "retrieve")
+graph = graph_builder.compile()
+
+# Use the RAG features from the graph
+result = graph.invoke({"question": "What is Chain of Hindsight ?"})
+
+print(f'Context: {result["context"]}\n\n')
+print(f'Answer: {result["answer"]}')
